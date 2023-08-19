5fc46fa5f37d14d1bf3b582283fb6826d648ebd2
Salbitt <writetosalonistephen@gmail.com>
2023-08-19 17:44:02 +0530

    commit message

--- train-api/index.js
+++ train-api/index.js
@@ -1,22 +1,63 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app= express()
-const trains =     {
-        name:"Rajdhani Express",
-        id:"A12"
+const trains=[
+    {
+        companyName : "Anja",
+        rollNo :"1",
+        ownerName:"Ram",
+        ownerEmail:"ram@gmail.com",
+        accesscode:"KIHLjn"
+    },
+    {
+        companyName : "Rathri",
+        rollNo :"2",
+        ownerName:"Ricky",
+        ownerEmail:"rick@gmail.com",
+        accesscode:"ANhbs"
     }
+]
 
+const accesscodes = [
+    {
+        companyName:"Anja",
+        rollNo:"1",
+        accesscode:"KIHLjn"
+    },
+    {
+        companyName:"Prati",
+        rollNo:"2",
+        accesscode:"Ansks"
+    },
+    {
+        companyName:"Rathri",
+        rollNo:"2",
+        accesscode:"ANhbs"
+    }
+]
+app.use(express.json())
 app.listen(5501,()=>{
     console.log('Running')
 })
 
-app.get('/trains',(req,res)=>{
-   
-    res.send(trains.name+" "+trains.id)
+app.post('/train/register',async(req,res)=>{
+   const company = req.body.companyName;
+   const roll = req.body.rollNo;
+   const access = req.body.accesscode;
+
+   const registerCode = {companyName:''+company,rollNo:''+roll,accesscode:''+access}
     
+   if(accesscodes.includes(registerCode))
+   {
+    res.send('All done')
+   }
+   else{
+    res.send(registerCode+' '+'Access denied')
+   }
 })
 
-app.post('/trains-list',(req,res))
-{
-    
-}
+
+
+app.post('/train-display',async(req,res)=>{
+    res.send('req.body')
+})
